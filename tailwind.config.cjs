8605e68d72523c650a29d4ea2e0152a170908d34
Frank Stallone III <frank@stallone.dev>
2022-08-22 21:04:27 -0400

    feat: ✨ Got the menu closer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: "var(--color-primary)",
